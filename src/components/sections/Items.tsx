bde3605fb03cae612deffc495532492b68d3c69a
Tester <test@email.com>
2025-03-23 21:50:41 -0400

    commit

--- src/components/sections/Items.tsx
+++ src/components/sections/Items.tsx
@@ -23,7 +23,7 @@ const categories: Category[] = [
         items: [
             {
                 title: "Helmet",
-                description: "Protects the head from impacts and projectiles. Enhances neck protection and style.",
+                description: "Protects the head sfrom impacts and projectiles. Enhances neck protection and style.",
                 icon: (
                     <svg class="w-full h-full" viewBox="0 0 24 24">
                         <path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
